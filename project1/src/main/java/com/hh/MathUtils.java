002dd1a8ee48070c44735009845e468025f4f49f
HetAxelor2021 <hch.axelor@gmail.com>
2021-10-06 15:24:58 +0530

    used  @BeforeEach

--- project1/src/main/java/com/hh/MathUtils.java
+++ project1/src/main/java/com/hh/MathUtils.java
@@ -9,5 +9,17 @@ public class MathUtils {
 		return Math.PI* radius * radius;
 	}
 	
+	public int subtract(int a, int b) {
+		return a-b;
+		
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	public int divide(int a, int b) {
+		return a/b;
+	}
+	
 	
 }
--- project1/src/test/java/com/hh/MathUtilsTest.java
+++ project1/src/test/java/com/hh/MathUtilsTest.java
@@ -2,14 +2,26 @@ package com.hh;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
+	int cachedValue = 0; 
+	MathUtils mathUtils;
+	
+	
+	@BeforeEach
+	
+	void init() {
+		mathUtils = new MathUtils();
+		
+	}
+	
 	@Test
 	void testAdd() {
-		MathUtils mathUtils = new MathUtils();
 		int expected = 2;
+//		cachedValue = 10;
 		int actual  = mathUtils.add(1, 1);
 		assertEquals(expected, actual,"The add method should add two numbers");
 		
@@ -17,11 +29,17 @@ class MathUtilsTest {
 	
 	@Test
 	void testComputeCircleRadius() {
-		MathUtils mathUtils = new MathUtils();
+//		MathUtils mathUtils = new MathUtils();
 		assertEquals(314.1592653589793,  mathUtils.computeCircleArea(10), "should return right cirlce area");
 		
 	}
 	
+	@Test
+	void testDivide() {
+//		MathUtils mathUtils = new MathUtils();
+		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0),"divide by zero should throw");
+		mathUtils.divide(1, 0);
+	}
 	
 
 }
